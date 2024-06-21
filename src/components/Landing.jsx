7e6cda75d5049fd8d24fdaaabc3d6f84ddea8fa2
sumit <mazumdarsumit3@gmail.com>
2024-06-21 12:26:14 +0530

    fix: changed some position of components

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,7 +5,7 @@ import { BiSolidNavigation } from "react-icons/bi";
 function Landing() {
   return (
     <div className="bg-primary lg:min-h-screen shadow-md">
-      <div className="p-5">
+      <div className="p-1">
         <h1 className="text-gray-50 flex items-center justify-center font-outfit text-[40px] lg:text-[300px]">
           Full Stack Developer
         </h1>
@@ -19,7 +19,7 @@ function Landing() {
           solutions.
         </p>
       </div>
-      <div className="p-10 grid grid-cols-2 gap-4">
+      <div className="p-3 grid grid-cols-2 gap-4">
         <div className="text-gray-50 mt-16">
           <button className="flex justify-center items-center gap-2 bg-black h-14 w-36 lg:w-40 rounded-full">
             Drop me a line{" "}
@@ -28,8 +28,8 @@ function Landing() {
             </div>
           </button>
         </div>
-        <div className="lg:flex lg:flex-row justify-center lg:gap-4 mt-16">
-          <p className=" text-gray-300 underline">Let’s get connected</p>
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-4 mt-16">
+          <p className=" text-gray-300 underline">Let’s connect</p>
           <div>
             <ul className="flex flex-col lg:flex-row justify-center items-center gap-2 text-gray-50 lg:text-3xl">
               <li className=" hover:text-gray-400 hover:text-xl hover:duration-200 lg:hover:text-4xl">
